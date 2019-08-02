66111552c92457c2ed7140fc274be0d5dd1a6d89
Krishna Srivastava <ksrivast@codeaurora.org>
2019-08-02 13:34:18 +0530

    msmnile_gvmq: add support for virtual ptp driver for LA guest
    Change-Id: I647b0779c44d67b18020b665b15c3532300aed85

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -99,6 +99,7 @@ BOARD_VENDOR_KERNEL_MODULES := \
     $(KERNEL_MODULES_OUT)/audio_stub.ko \
     $(KERNEL_MODULES_OUT)/audio_native.ko \
     $(KERNEL_MODULES_OUT)/audio_machine_msmnile.ko \
+    $(KERNEL_MODULES_OUT)/ptp_virtual.ko \
     $(KERNEL_MODULES_OUT)/emac_dwc_eqos.ko \
     $(KERNEL_MODULES_OUT)/hsi2s.ko \
     #$(KERNEL_MODULES_OUT)/rdbg.ko
