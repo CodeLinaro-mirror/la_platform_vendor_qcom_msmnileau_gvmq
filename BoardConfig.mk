4096089707d8ea6679b5ab0d59912337bbb5b497
Yingjie Zhu <yingzhu@codeaurora.org>
2019-08-06 18:21:09 +0800

    msmnile_gvmq: Restore Bluetooth
    (1) fstab.qcom
    Mount /dev/block/vdf into /bt_firmware
    
    (2) system.prop
    Set "persist.bt.clock_boottime_alarm" as false because
    clock CLOCK_BOOTTIME_ALARM is not supported in GVM.
    
    Note: this change is for reverting partial of change:
    426e55b476f3bb1172a4eaf8f0f821022c83220f
    
    Change-Id: I0b76e42b031ff4a78bd7996579454d897ca40a86

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_USES_IOPHAL := true
 
 # Some framework code requires this to enable BT
 BOARD_HAVE_BLUETOOTH := true
-BOARD_USES_WIPOWER := true
+BOARD_USES_WIPOWER := false
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common
 
 USE_OPENGL_RENDERER := true
