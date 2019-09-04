331a13c78e2f60bb7e32565aa62cf6b28498bde4
Derek Chen <chenche@codeaurora.org>
2019-09-04 00:16:55 -0700

    msmnileau_gvmq: fastforward audio changes for gvm
    Fastforward audio changes for GVM with metal.
    
    Change-Id: I98914b8efc02afa49878df353d88a4e5852de505

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -90,7 +90,7 @@ PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/msmnile/system_properties.x
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # Audio configuration file
--include $(TOPDIR)hardware/qcom/audio/configs/msmnile_au/msmnile_au.mk
+-include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/msmnile_au/msmnile_au.mk
 
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
