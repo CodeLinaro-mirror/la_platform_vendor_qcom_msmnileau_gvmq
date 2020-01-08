c8e686ddb80556fa967963036ee45933ee222249
Singa Reddy Dasari <singredd@codeaurora.org>
2020-01-08 16:24:11 +0530

    Removed ContextHub Hal and service for auto_gvmq device
    Change-Id: I04c55d0f3a3f2af328988c3f3d472a4916a7a96a
    Signed-off-by: Singa Reddy Dasari <singredd@codeaurora.org>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -163,10 +163,6 @@ PRODUCT_PACKAGES += \
 # MSM IRQ Balancer configuration file
 PRODUCT_COPY_FILES += device/qcom/msmnile/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# Context hub HAL
-PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
 
 # MIDI feature
 PRODUCT_COPY_FILES += \
