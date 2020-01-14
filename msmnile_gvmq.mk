dfbb409064bc4c8cef55949c26de3491394ac290
Abhiprayah Tiwari <abhipray@codeaurora.org>
2020-01-14 11:08:47 +0530

    msmnile_gvmq: Change device manifest to gvmq directory
    Used to pick msmnile_au manifest earlier. Copying the same
    to gvmq directory and using that now.
    
    Change-Id: Id1738db5c1b2074960dc39bd64409883f7bb04ae

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -147,10 +147,10 @@ PRODUCT_PACKAGES += \
 
 
 
-DEVICE_MANIFEST_FILE := device/qcom/msmnile_au/manifest.xml
+DEVICE_MANIFEST_FILE := device/qcom/msmnile_gvmq/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
-DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/msmnile_au/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/qcom/msmnile_au/vendor_framework_compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/msmnile_gvmq/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := device/qcom/msmnile_gvmq/vendor_framework_compatibility_matrix.xml
 
 
 #ANT+ stack
