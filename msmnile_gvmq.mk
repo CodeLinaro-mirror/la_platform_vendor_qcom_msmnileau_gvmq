d9cb78536cc13720b8f053520050d18b2ab817ce
Huajie Wu <whj@codeaurora.org>
2020-06-06 01:43:31 +0800

    msmnile_gvmq: Remove graphics HIDL setting
    Remove graphics HIDL in msmnile_gvmq.mk.
    
    Reason: graphics HIDL is installed in
    'hardware/qcom/display/config/display-product.mk'
    
    Change-Id: Ia270a9836c748fe2f6bff5277f01c919c990f3c3

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -227,23 +227,6 @@ PRODUCT_PACKAGES += android.hardware.automotive.evs@1.0-service \
     android.automotive.evs.manager@1.0 \
     android.hardware.automotive.audiocontrol@1.0-service \
 
-PRODUCT_PACKAGES += \
-     libion \
-     android.hardware.graphics.mapper@2.0 \
-     android.hardware.graphics.mapper@2.0-impl \
-     android.hardware.graphics.mapper@2.0-impl-qti-display \
-     vendor.qti.hardware.display.allocator@1.0-service \
-     android.hardware.graphics.allocator@2.0 \
-     android.hardware.graphics.allocator@2.0-impl \
-     android.hardware.graphics.allocator@2.0-service \
-     android.hardware.graphics.composer@2.1 \
-     android.hardware.graphics.composer@2.1-impl \
-     android.hardware.graphics.composer@2.1-service \
-     android.hardware.memtrack@1.0-impl \
-     android.hardware.memtrack@1.0-service \
-     android.hardware.light@2.0-impl \
-     android.hardware.light@2.0-service \
-
 #add vndservicemanager
 PRODUCT_PACKAGES += vndservicemanager
 
