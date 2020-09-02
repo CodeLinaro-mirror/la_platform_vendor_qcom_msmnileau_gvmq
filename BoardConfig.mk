e160cb9a0b2cc4aaff279a4c4d6fe2fc76358606
vagdhan kumar kanukurthi <vagdhank@codeaurora.org>
2020-09-02 19:09:31 +0530

    msmnile_gvmq: Add boot header version 2 supoort
    Update kernel command line and Add boot header version 2
    supoort for Gen3_HQX(RB).
    
    Change-Id: I5f792f224242e58f26e3ba4acacb5358da929d15
    Signed-off-by: Venkata Rao Kakani <vkakani@codeaurora.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,8 @@ TARGET_USES_IOPHAL := true
 
 BUILD_BROKEN_DUP_RULES := true
 
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
 -include $(QCPATH)/common/msmnile_gvmq/BoardConfigVendor.mk
 
 # Some framework code requires this to enable BT
@@ -41,7 +43,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/qcom/common/automotive
 USE_OPENGL_RENDERER := true
 BOARD_USE_LEGACY_UI := true
 # Set Header version for bootimage
-BOARD_BOOTIMG_HEADER_VERSION := 3
+BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 
 # Defines for enabling A/B builds
@@ -106,7 +108,8 @@ BOARD_VENDOR_KERNEL_MODULES += $(shell ls $(KERNEL_MODULES_OUT)/*.ko)
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
 TARGET_USES_QCOM_BSP := false
-BOARD_KERNEL_CMDLINE := console=ttyAMA0 earlycon=pl011,0x1c090000 debug user_debug=31 loglevel=9 print-fatal-signals=1 no_console_suspend androidboot.console=ttyAMA0 androidboot.hardware=qcom androidboot.selinux=enforcing androidboot.memcg=1 root=/dev/vda init=/init swiotlb=2048 gvmip=192.168.1.3 androidboot.usbcontroller=a600000.dwc3 androidboot.recover_usb=1
+
+BOARD_KERNEL_CMDLINE := console=ttyAMA0 earlycon=pl011,0x1c090000 debug user_debug=31 loglevel=9 print-fatal-signals=1 androidboot.console=ttyAMA0 androidboot.hardware=qcom androidboot.selinux=permissive androidboot.memcg=1 init=/init swiotlb=2048 androidboot.usbcontroller=a600000.dwc3 androidboot.recover_usb=1 androidboot.serialno=1234567
 
 BOARD_EGL_CFG := device/qcom/$(TARGET_BOARD_PLATFORM)/egl.cfg
 
