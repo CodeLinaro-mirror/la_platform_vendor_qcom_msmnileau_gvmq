11e5d2736768e73bd9c62b25b196120a9f91e8b6
Pankaj Kumar <pankkuma@codeaurora.org>
2020-09-03 11:33:19 +0530

    msmnile_gvmq: Removed Light HAL Package for API 30
    Change-Id: I9b366d9b99e8e94089291872b8463ca6daed582a

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -241,8 +241,6 @@ PRODUCT_PACKAGES += android.hardware.automotive.evs@1.0-service \
 PRODUCT_PACKAGES += android.hardware.health@2.1-service \
                     android.hardware.health@2.1-impl \
                     android.hardware.health@2.1-impl.recovery \
-                    android.hardware.light@2.0-impl \
-                    android.hardware.light@2.0-service
 #add vndservicemanager
 PRODUCT_PACKAGES += vndservicemanager
 
