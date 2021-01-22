c7b49dac310ced75a556946f3bcf1e4b08212499
Tabassum Tabassum <ttabassu@codeaurora.org>
2021-01-22 11:26:39 +0530

    msmnile_gvmq : updating gnss HAL version
    updating gnss HAL version
    
    Change-Id: I6741f2da8525b8f637e1e4cab66603ec8a0731b9

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -243,6 +243,9 @@ PRODUCT_PACKAGES += android.hardware.health@2.1-service \
                     android.hardware.health@2.1-impl.recovery \
                     android.hardware.dumpstate@1.1-service.example \
                     android.hardware.thermal@2.0-service.mock \
+
+PRODUCT_PACKAGES += android.hardware.gnss@2.0-service
+
 #add vndservicemanager
 PRODUCT_PACKAGES += vndservicemanager
 
