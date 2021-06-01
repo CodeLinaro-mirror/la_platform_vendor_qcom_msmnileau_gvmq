424e10453223073d49d062a026e93ff1a368792e
Abhiprayah Tiwari <abhipray@codeaurora.org>
2021-06-01 22:35:04 +0530

    msmnile_gvmq: Keep testscripts file only for userdebug builds
    Keep the file only for userdebug builds.
    
    Change-Id: I1b719eccddd1cc4f24f792c79ff181b81c12e4de

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -146,6 +146,11 @@ endif
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
 
+ifneq ($(strip $(TARGET_BUILD_VARIANT)),user)
+PRODUCT_COPY_FILES += \
+    device/qcom/qssi/init.qcom.testscripts.sh:system/etc/init.qcom.testscripts.sh
+endif
+
 # Video codec configuration files
 ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
 PRODUCT_COPY_FILES += device/qcom/msmnile/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
