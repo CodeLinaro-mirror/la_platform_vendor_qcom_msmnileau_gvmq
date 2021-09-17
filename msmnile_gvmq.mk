a01e8b198a65830c144cac5447251b9213bd5bbb
Tabassum Tabassum <ttabassu@codeaurora.org>
2021-09-17 11:40:53 +0530

    msmnile_gvmq: Disabling virtual AB.
    File contents are not persistent across reboots
    with dm-verity disabled, its because of virtual
    AB.
    
    Change-Id: I00d91d8e72bca08c7fb4f38b214fd32f5aa6354b

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -36,7 +36,7 @@ ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
   ENABLE_AB = true
   # Enable virtual-ab by default
   ifeq ($(ENABLE_AB), true)
-    ENABLE_VIRTUAL_AB ?= true
+    ENABLE_VIRTUAL_AB ?= false
   endif
   ifeq ($(ENABLE_VIRTUAL_AB), true)
     $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
