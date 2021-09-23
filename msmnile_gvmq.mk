6b79162cab7971342cdbe45a67b45559ac1cb267
Narsinga Rao Chella <nrchella@codeaurora.org>
2021-09-23 16:06:02 +0530

    msmnile_gvmq: Add nbaio for avenhancement
    Add nbaio for avenhancemnet otherwise will cause
    compress-offload usecase to fail for certain formats
    
    Suggested-by: Karthik D K
    
    Change-Id: I4e0a85df8ab3fa3168d263af4a6680ca333f3ec0
    Signed-off-by: Narsinga Rao Chella <nrchella@codeaurora.org>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -193,6 +193,9 @@ AUDIO_DLKM += audio_native.ko
 AUDIO_DLKM += audio_machine_msmnile.ko
 PRODUCT_PACKAGES += $(AUDIO_DLKM)
 
+#add libnbaio for avenhancement
+PRODUCT_PACKAGES += libnbaio
+
 PCIE_DLKM := pci_msm_drv
 PRODUCT_PACKAGES += $(PCIE_DLKM)
 
