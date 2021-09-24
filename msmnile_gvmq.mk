bceda35cacd0d34a5b35d3ac924c7eab5ab3ea3a
Susan Wang <yingshuw@codeaurora.org>
2021-09-24 17:46:53 -0400

    msmnile_gvmq: remove audio content
    Migrate audio related definitions to audio components.
    As part of audio self-containment, these defs were
    migrated to audio_vendor_product.mk and
    audio_system_product.mk.
    The softlink for audio_vendor_product.mk was created in
    vendor/qcom/defs/product-defs/vendor/ .
    the softlink for audio_system_product.mk was created in
    vendor/qcom/defs/product-defs/system/ .
    
    Change-Id: I4bc29c571a8d7d3c46f3918d020d882d63baee5b

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -9,7 +9,6 @@ BOARD_AVB_ENABLE := true
 BOARD_USES_QCNE := false
 TARGET_BOARD_AUTO := true
 TARGET_USES_AOSP := true
-TARGET_USES_AOSP_FOR_AUDIO := true
 TARGET_USES_QCOM_BSP := false
 TARGET_NO_TELEPHONY := true
 TARGET_USES_QTIC := false
@@ -169,9 +168,6 @@ endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 #PRODUCT_COPY_FILES += hardware/qcom/media/conf_files/msmnile/system_properties.xml:$(TARGET_COPY_OUT_VENDOR)/etc/system_properties.xml
 
 PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
-
-# Audio configuration file
--include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/msmnile_au/msmnile_au.mk
 
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
