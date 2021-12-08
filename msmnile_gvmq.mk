7b86d693cde727c86eba9b6e104deeb08c722301
Abhiprayah Tiwari <quic_abhipray@quicinc.com>
2021-12-09 02:00:45 +0530

    Add gsi_keys in first stage ramdisk
    Currently GSI flashing and boot up procedure requires AVB
    authentication disablement. This change adds GSI avb keys in ramdisk
    and it will be matched against flashed GSI during first stage init.
    
    Change-Id: I11b9f1689ace5d585108f28752f67bcb699b194d

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -343,6 +343,9 @@ PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
 PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
 PRODUCT_PROPERTY_OVERRIDES += ro.hardware.camera=v4l2
 PRODUCT_PACKAGES += camera.v4l2
+
+PRODUCT_PACKAGES += qcar-gsi.avbpubkey
+
 # privapp-permissions whitelisting (To Fix CTS :privappPermissionsMustBeEnforced)
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
 
