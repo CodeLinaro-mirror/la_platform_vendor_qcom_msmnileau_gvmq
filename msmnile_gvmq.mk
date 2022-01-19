cc5f12933cfa8b81c52e0725f4304ebf0f9f8d69
Nasif <quic_nnooru@quicinc.com>
2022-01-19 14:28:47 +0530

    VTS/HQX1.2.1 : Disabled diag router in user build
    Disabled diag router,android.hardware.bluetooth.audio
    This fix is needed for below VTS to fix
    run vts -m vts_treble_vintf_vendor_test#
    DeviceManifest/SingleManifestTest#HalsAreServed/0
    DeviceManifest/SingleManifestTest#InterfacesAre
    Released/0
    
    Change-Id: Idc044e872b928e917fecb2208c9d70d05976b509

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -18,6 +18,8 @@ TARGET_FWK_SUPPORTS_AV_VALUEADDS := true
 TARGET_USES_AOSP_FOR_WLAN := false
 ENABLE_CAR_POWER_MANAGER := true
 VPP_TARGET_USES_SERVICE := NO
+TARGET_HAS_DIAG_ROUTER := true
+
 
 # Dynamic-partition enabled by default
 BOARD_DYNAMIC_PARTITION_ENABLE := true
@@ -136,6 +138,12 @@ PRODUCT_PACKAGES += libGLES_android
 # diag-router
 TARGET_HAS_DIAG_ROUTER := true
 TARGET_NOT_SUPPORT_VULKAN := true
+
+# diag-router no there for user
+ifeq ($(strip $(TARGET_BUILD_VARIANT)),user)
+    TARGET_HAS_DIAG_ROUTER := false
+endif
+
 
 -include $(QCPATH)/common/config/qtic-config.mk
 
