483dd118a40a8f76bf490d66b06fbb0e34e34660
vagdhan kumar kanukurthi <quic_vagdhank@quicinc.com>
2022-03-29 11:49:05 +0530

    msmnile_gvmq : Enable vendor_boot
    Enable vendor boot support for gvm.
    
    Change-Id: Ie58c9ba515bc5f677757cd2e7a0a1e9353daf4e6

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -54,13 +54,13 @@ ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
   # Enable System_ext
   PRODUCT_BUILD_SYSTEM_EXT_IMAGE := true
   PRODUCT_PACKAGES += fastbootd
-
   PRODUCT_BUILD_SYSTEM_OTHER_IMAGE := false
   PRODUCT_BUILD_PRODUCT_IMAGE := false
   PRODUCT_BUILD_PRODUCT_SERVICES_IMAGE := false
   PRODUCT_BUILD_CACHE_IMAGE := false
   PRODUCT_BUILD_RAMDISK_IMAGE := true
   PRODUCT_BUILD_USERDATA_IMAGE := true
+  PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
 
   # Using sha256 for dm-verity partitions.
   # system, system_other, system_ext and product.
@@ -69,11 +69,9 @@ ifeq ($(strip $(BOARD_DYNAMIC_PARTITION_ENABLE)),true)
   BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm sha256
 
   ifeq ($(ENABLE_AB), true)
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
   else
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
-    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom
+    PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
   endif
 endif #BOARD_DYNAMIC_PARTITION_ENABLE
 TARGET_DEFINES_DALVIK_HEAP := true
