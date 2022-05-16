758aedb53ac1cb56a05ef57c7f28de635dd6735e
Rongjing Liao <quic_liaor@quicinc.com>
2022-05-16 11:59:40 +0800

    msmnile_gvmq: add flag to support dual wlan
    Add flag to support dual wlan on gvm platform.
    
    Change-Id: I5a5dd4058524ab1b6b8aa8ddc58522aa6544f67e
    CRs-Fixed: 3331223

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -31,6 +31,7 @@ TARGET_USES_AOSP_FOR_WLAN := true
 ENABLE_CAR_POWER_MANAGER := true
 VPP_TARGET_USES_SERVICE := NO
 BOARD_HAVE_DUAL_BLUETOOTH := true
+TARGET_SUPPORT_DUAL_WLAN := true
 
 # Mismatch in the uses-library tags between build system and the manifest leads
 # to soong APK manifest_check tool errors. Enable the flag to fix this.
