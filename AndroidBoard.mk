891bf0bd3d7d34e2ca6b820ffe89fd0daec906e8
Singa Reddy Dasari <quic_singredd@quicinc.com>
2022-07-11 11:01:45 +0530

    [msmnile_gvmq] Bringup changes for HMI
    Intial bringup changes for autogvm with T+5.15.
    
    Change-Id: I7b2ccf972e78f84e07e23393123cef942af4610f

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -39,7 +39,7 @@ TARGET_KERNEL_MAKE_ENV += HOSTCFLAGS="-I$$(pwd)/kernel/msm-4.14/include/uapi -I/
 TARGET_KERNEL_MAKE_ENV += HOSTLDFLAGS="-L/usr/lib -L/usr/lib/x86_64-linux-gnu -fuse-ld=lld"
 
 #include $(TARGET_KERNEL_SOURCE)/AndroidKernel.mk
-include device/qcom/kernelscripts/kernel_definitions.mk
+#include device/qcom/kernelscripts/kernel_definitions.mk
 $(TARGET_PREBUILT_KERNEL): $(DTC) $(UFDT_APPLY_OVERLAY)
 
 $(INSTALLED_KERNEL_TARGET): $(TARGET_PREBUILT_KERNEL) | $(ACP)
