cbbfdef71c18ade078d0d69def7d54b3f481a2c4
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2022-10-14 11:58:02 +0530

    msmnile_gvmq : Enabled GAS properties
    Change-Id: I2584fa6676cfd8c6a5238ccfb3daa5ae1cba991e

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -295,6 +295,11 @@ endif
 
 TARGET_MOUNT_POINTS_SYMLINKS := false
 
+#Copy supported features list
+ifeq ($(TARGET_USES_GAS),true)
+PRODUCT_COPY_FILES += device/qcom/msmnile_gvmq/msmnile_gvmq_features.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/msmnile_gvmq_features.xml
+endif
+
 # Camera configuration file. Shared by passthrough/binderized camera HAL
 PRODUCT_PACKAGES += camera.device@3.2-impl
 PRODUCT_PACKAGES += camera.device@1.0-impl
