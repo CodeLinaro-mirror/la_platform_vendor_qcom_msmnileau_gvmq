d563f1c04853aa89dc56ad8eebc59f0b2c6f00d6
Venkata Rao Kakani <quic_vkakani@quicinc.com>
2022-11-17 19:25:01 +0530

    msmnile_gvmq: Remove redundent softlinks
    Change-Id: Iab178976e3c543778d100460a6915df7c43b2f0a
    Signed-off-by: Venkata Rao Kakani <quic_vkakani@quicinc.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,9 +186,6 @@ else
 	TARGET_USES_UNCOMPRESSED_KERNEL := true
 endif
 
-KERN_PATH := kernel_platform/msm-kernel/
-$(shell if ! [ -L $(KERN_PATH)gen_headers_arm64.bp ]; then rm $(KERN_PATH)gen_headers_arm64.bp && ln -s gen_headers_arm64_auto.bp $(KERN_PATH)gen_headers_arm64.bp; fi)
-$(shell if ! [ -L $(KERN_PATH)gen_headers_arm.bp ]; then rm $(KERN_PATH)gen_headers_arm.bp && ln -s gen_headers_arm_auto.bp $(KERN_PATH)gen_headers_arm.bp; fi)
 
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
 MAX_EGL_CACHE_SIZE := 2048*1024
