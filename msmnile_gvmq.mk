95802a0630e2550f683fa4273c66eefc035ef7f0
Tabassum Tabassum <quic_ttabassu@quicinc.com>
2022-11-24 15:14:15 +0530

    msmnile_gvmq : Disabled Virtual AB to fix remount issue
    Change-Id: Ib56a3bd5167dbbd7e1362bb04dc0900fe76edd99

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -18,7 +18,7 @@ ALLOW_MISSING_DEPENDENCIES := true
   ENABLE_AB ?= true
   # Enable virtual-ab by default
   ifeq ($(ENABLE_AB), true)
-    ENABLE_VIRTUAL_AB ?= true
+    ENABLE_VIRTUAL_AB ?= false
   endif
   ifeq ($(ENABLE_VIRTUAL_AB), true)
     $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
