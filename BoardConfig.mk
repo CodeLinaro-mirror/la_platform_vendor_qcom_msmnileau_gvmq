e6f9ed5621fb7f8e4e4041de5a10183ec1fe8864
Swapneel Mashalkar <quic_smashalk@quicinc.com>
2023-01-27 22:31:54 +0530

    [DNM]: Lemans WA to enable console
    Change-Id: I631089c496c1133562dee70c3ae4b66cd945d629
    Signed-off-by: Swapneel Mashalkar <quic_smashalk@quicinc.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,12 +174,12 @@ TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API :=true
 TARGET_USES_QCOM_BSP := false
 
-BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.selinux=enforcing androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.recover_usb=1
+BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.selinux=permissive androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.recover_usb=1
 
 BOARD_KERNEL_CMDLINE := debug user_debug=31 loglevel=9 print-fatal-signals=1  init=/init swiotlb=4096  kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image
 
 ifeq ($(TARGET_CONSOLE_ENABLED),true)
-BOARD_KERNEL_CMDLINE += console=hvc0,115200
+BOARD_KERNEL_CMDLINE += earlycon=pl011,0x1c090000
 BOARD_BOOTCONFIG += androidboot.console=ttyAMA0
 else
 ifeq ($(TARGET_CONSOLE_ENABLED),false)
