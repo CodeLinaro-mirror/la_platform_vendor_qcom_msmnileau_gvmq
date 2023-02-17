77380bfb2b738a2176c2c3753a50f9360040288c
Ashok Mutyala <quic_amutyala@quicinc.com>
2023-02-17 12:34:47 +0530

    msmnile_gvmq: update kernel cmd line with loop.max_part
    - Required for creating public volume
    - fix CtsScopedStorageDeviceOnlyTest
    
    Change-Id: I34957bc30f04442d2a16e50fc02a701df9b6b400
    Signed-off-by: Ashok Mutyala <quic_amutyala@quicinc.com>
    (cherry picked from commit 18c34638ddeccfaf956bb32f7b305bc83a13e4af)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ TARGET_USES_QCOM_BSP := false
 
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.selinux=permissive androidboot.memcg=1 androidboot.recover_usb=1
 
-BOARD_KERNEL_CMDLINE := debug user_debug=31 loglevel=9 print-fatal-signals=1  init=/init swiotlb=4096  kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE := debug user_debug=31 loglevel=9 print-fatal-signals=1  init=/init swiotlb=4096  kpti=0 pcie_ports=compat firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
 
 ifeq ($(TARGET_CONSOLE_ENABLED),true)
 BOARD_KERNEL_CMDLINE += console=hvc0,115200
