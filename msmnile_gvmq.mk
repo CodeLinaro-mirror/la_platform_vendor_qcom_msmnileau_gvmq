c4759eceeee02c87020b25b8108a4a87d322825e
Gopala Krishna Nuthaki <quic_gnuthaki@quicinc.com>
2023-02-21 17:12:39 +0530

    thermal: Remove thermal-engine and thermal-hal mock services
    - systemctl runs thermal-engine service during boot.
      Remove thermal-engine service enablement from
      init.target.rc file.
    - Remove thermal-hal mock service configuration.
    
    Change-Id: Ifbe84d001f76924b37f9ff63f64ec886459e4803
    Signed-off-by: Gopala Krishna Nuthaki <quic_gnuthaki@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -373,8 +373,7 @@ PRODUCT_PACKAGES += \
 #PRODUCT_PACKAGES += android.hardware.automotive.audiocontrol@1.0-service
 
 PRODUCT_PACKAGES += android.hardware.health-service.example \
-                    android.hardware.dumpstate-service.example \
-                    android.hardware.thermal@2.0-service.mock
+                    android.hardware.dumpstate-service.example
 
 PRODUCT_PACKAGES += android.hardware.gnss@2.0-service
 PRODUCT_PACKAGES += qcar-gsi.avbpubkey
