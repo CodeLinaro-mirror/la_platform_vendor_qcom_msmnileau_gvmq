60423988c79ad45f0f5988c8fec1068ae3100e62
Anthony Mah <quic_amah@quicinc.com>
2023-02-22 00:25:57 -0800

    msmnileau_gvmq: Disable 32 bit app support
    set flag DEVICE_SUPPORTS_64_BIT_APPS_ONLY to true
    
    Change-Id: I3da1b19ea33e993ead9626cff3a64ac745d6fb74
    Signed-off-by: Asmeer Mohammed <quic_asmemoha@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -50,6 +50,7 @@ TARGET_USES_AOSP_FOR_WLAN := true
 BOARD_HAS_QCOM_WLAN := true
 ENABLE_CAR_POWER_MANAGER := true
 VPP_TARGET_USES_SERVICE := NO
+DEVICE_SUPPORTS_64_BIT_APPS_ONLY := true
 
 #Enable Userspace Restart
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
