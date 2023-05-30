a146400888dc44042136874d6a3ccb4fd449d352
Asmeer Mohammed <quic_asmemoha@quicinc.com>
2023-05-30 10:09:04 +0530

    Revert "msmnileau_gvmq: Disable 32 bit app support"
    This reverts commit a0dad57c40ba43bdaaf07cb06ac04fd1736c0a57.
    
    Change-Id: I39e2ada51a91e65e4c9bca17669c85c326225250

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -56,7 +56,6 @@ ENABLE_CAR_POWER_MANAGER := true
 VPP_TARGET_USES_SERVICE := NO
 TARGET_GVMGH_SPECIFIC := false
 TARGET_USES_RRO := true
-DEVICE_SUPPORTS_64_BIT_APPS_ONLY := true
 
 #Enable Userspace Restart
 $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
