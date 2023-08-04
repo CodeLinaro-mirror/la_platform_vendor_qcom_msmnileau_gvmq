e57381ad3392236f581ae33492f4cb67a53e8939
Richard J K <quic_rjk@quicinc.com>
2023-08-05 04:35:58 +0530

    Revert "Added property for Target Board Suffix"
    This reverts commit 7185538b7315891fe2bc3bfaaaabadca059dfcd6.
    
    Change-Id: Ia32cbdb88f79bfaa2bfaa51b339dd0cd8579ade0
    Signed-off-by: Richard J K <quic_rjk@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -224,10 +224,6 @@ PRODUCT_PACKAGES += update_engine \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.bootctrl.enable=true
 
-# Board suffix property
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.board.suffix=$(TARGET_BOARD_SUFFIX)
-
 PRODUCT_HOST_PACKAGES += \
 	brillo_update_payload
 
