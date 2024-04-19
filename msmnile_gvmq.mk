3101535e330ffdeff7bede87a356f2d6a6ec2ec2
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-04-18 22:15:19 -0700

    Remove GAS Compilation at Vendor
    Change-Id: Ib276670d40fc64522614b4241e04eb525b336c6a
    Test: HY11 && HY11-USER build
    Signed-Off-By: Ashok Mutyala <quic_amutyala@quicinc.com>
    (cherry picked from commit 323cb20dde2b759c5d272fc8d8e693da801ba0fb)

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -91,10 +91,6 @@ BOARD_AVB_ENABLE := true
 BOARD_USES_QCNE := false
 TARGET_BOARD_AUTO := true
 TARGET_USES_AOSP := true
-#TODO(amutyala) to revert this once QSSI 15 component created
-ifeq (,$(filter VanillaIceCream V 35, $(PLATFORM_VNDK_VERSION)))
-  TARGET_USES_GAS := true
-endif
 TARGET_USES_QCOM_BSP := false
 TARGET_NO_TELEPHONY := true
 TARGET_USES_QTIC := false
