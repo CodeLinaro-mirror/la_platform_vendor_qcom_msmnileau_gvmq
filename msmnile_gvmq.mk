3d7db3fde7f0bc4f4afb63f5e119ea525ea29d91
Zhongyue Gao <quic_zhongao@quicinc.com>
2024-05-09 11:13:50 +0800

    msmnile_gvmq: Add hardware/qcom/wlan/qcwcn namespace
    With libwifi-hal-qcom migrated to Android.bp with soong namespace
    the namespace should be added to PRODUCT_SOONG_NAMESPACES as well.
    
    CRs-Fixed: 3844210
    Change-Id: I38d2636e72bbf9f509495bc1272e1dc86909f135
    Signed-off-by: Zhongyue Gao <quic_zhongao@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -192,6 +192,8 @@ PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
 PRODUCT_BUILD_VENDOR_DLKM_IMAGE := true
 PRODUCT_BUILD_SYSTEM_DLKM_IMAGE := true
 
+PRODUCT_SOONG_NAMESPACES += hardware/qcom/wlan/qcwcn
+
 #Using sha256 for dm-verity partitions.
 #system, system_other, system_ext and product.
 #TODO @asmemoha cleanup syste/system_ext avb configs. No impact either way
