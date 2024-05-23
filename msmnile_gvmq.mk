3bdb03b068ef4ea12b0060b578bb9022497f8fd4
Yanfei Zhou <quic_zyanfei@quicinc.com>
2024-05-23 12:13:11 +0530

    msmnile_gvmq: Enable dual Bluetooth
    Set config BOARD_HAVE_DUAL_BLUETOOTH as true
    
    Change-Id: I3fad98173c9f9ff99ba8755b5f9f5d002ee1cbbd
    Signed-off-by: Yanfei Zhou <quic_zyanfei@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -79,6 +79,15 @@ TARGET_SUPPORT_DUAL_WLAN := true
 # to soong APK manifest_check tool errors. Enable the flag to fix this.
 RELAX_USES_LIBRARY_CHECK := true
 
+# Enable dual Bluetooth
+ifeq ($(TARGET_FWK_SUPPORTS_FULL_VALUEADDS),true)
+ifeq ($(TARGET_BOARD_TYPE),auto)
+ifeq ($(ENABLE_HYP),true)
+BOARD_HAVE_DUAL_BLUETOOTH := true
+endif
+endif
+endif
+
 ifneq ($(TARGET_BOARD_DERIVATIVE_SUFFIX), _ext4)
 ifeq ($(ENABLE_AB), true)
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_AB_dynamic_partition_variant.qti:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
