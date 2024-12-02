c1d62c84ea837ab010a313888e81f3b5a1dee588
Ashok Mutyala <quic_amutyala@quicinc.com>
2024-12-02 16:38:56 +0530

    msmnile_gvmq: Add default android.hardware.broadcastradio
    Add default broadcastradio service to fix following runtime error:
    E AndroidRuntime: java.lang.RuntimeException:
    Unable to create service com.android.car.radio.service.RadioAppService:
    java.lang.NullPointerException: RadioManager could not be loaded
    
    Change-Id: I881aae3753cf026ba0c19ace11ad73a4c5e13a80
    Signed-off-by: Ashok Mutyala <quic_amutyala@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -387,7 +387,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
+    android.hardware.broadcastradio-service.default
+
+PRODUCT_COPY_FILES += \
+     frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml
 
 # MSM IRQ Balancer configuration file
 #PRODUCT_COPY_FILES += device/qcom/msmnile/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
