794d3f72897757c1933afc40468e5991b38519d1
Samreen Imran Bagawan <quic_sbagawan@quicinc.com>
2024-12-26 18:53:10 +0530

    msmnile_gvmq : Removing unused product properties.
    To resolve AVC Denial for property=persist.timed.enable  and property=persist.fuse_sdcard.
    
    Change-Id: I3e3655abbc5623a6c74b79c7ed978a80491b8ffa
    Signed-off-by: Samreen Imran Bagawan <quic_sbagawan@quicinc.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -645,9 +645,6 @@ PRODUCT_VENDOR_PROPERTIES += media.stagefright.enable-player=true \
 PRODUCT_VENDOR_PROPERTIES += ro.vendor.use_data_netmgrd=true \
                             persist.vendor.data.mode=concurrent
 
-#system props for time-services
-PRODUCT_VENDOR_PROPERTIES += persist.timed.enable=true
-
 # system prop for opengles version
 # 196608 is decimal for 0x30000 to report version 3
 # 196609 is decimal for 0x30001 to report version 3.1
@@ -659,9 +656,6 @@ PRODUCT_VENDOR_PROPERTIES += bt.max.hfpclient.connections=1
 
 # system prop to turn on CdmaLTEPhone always
 PRODUCT_VENDOR_PROPERTIES += telephony.lteOnCdmaDevice=1
-
-#Simulate sdcard on /data/media
-PRODUCT_VENDOR_PROPERTIES += persist.fuse_sdcard=true
 
 BOARD_HAVE_QCOM_BLE_AUDIO := true
 
