e4daaeb33e9fea72dc12915705c6cc6f6dfa438f
Kiran, Pachava <kpachava@qti.qualcomm.com>
2025-12-20 08:49:29 +0530

    [msmnile_gvmq]: Enable postinstall step for OTA updates
    This change enables the postinstall step for OTA updates
    change propagated from #818253571
    
    Change-Id: I17bca9a4e95f70443471825a6cb6c8c19e00322c
    Signed-off-by: Kiran, Pachava <kpachava@qti.qualcomm.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -766,6 +766,13 @@ ifeq ($(TARGET_SINGLE_TREE), true)
   PRODUCT_PACKAGES += vendor.qti.qesdsys
 endif
 
+ifneq ( , $(filter bp4a cp2a, $(TARGET_RELEASE_PLATFORM)))
+AB_OTA_POSTINSTALL_CONFIG += \
+               RUN_POSTINSTALL_vendor=true \
+               FILESYSTEM_TYPE_vendor=ext4 \
+               POSTINSTALL_OPTIONAL_vendor=true
+endif
+
 ifeq ($(filter $(PLATFORM_VERSION), 15 VanillaIceCream V),$(PLATFORM_VERSION))
 TARGET_SUPPORTS_VM_AUTO := false
 else
