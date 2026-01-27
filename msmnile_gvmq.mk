529e6d7d52300d73fea892324ea4b2d8dfb392e5
Sravani Mada <smada@qti.qualcomm.com>
2026-01-27 22:29:39 +0530

    msmnile_gvmq: Fix for SGT bootup issue
    Added car_system.mk changes
    
    Change-Id: I82e9d4475a4971a5d12da8ce17d6b27807555e63
    Signed-off-by: Sravani Mada <smada@qti.qualcomm.com>

--- msmnile_gvmq.mk
+++ msmnile_gvmq.mk
@@ -232,7 +232,14 @@ PRODUCT_PROPERTY_OVERRIDES  += \
 
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
 
-$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+ifeq (true,$(call math_gt_or_eq,$(PLATFORM_SDK_VERSION),36))
+  $(call inherit-product, device/qcom/qssi_au/qssi_au_system_generic.mk)
+  $(call inherit-product, packages/services/Car/car_product/build/car_generic_system.mk)
+  $(call inherit-product, packages/services/Car/car_product/build/car_system_ext.mk)
+  $(call inherit-product, packages/services/Car/car_product/build/car_product.mk)
+else
+  $(call inherit-product, packages/services/Car/car_product/build/car.mk)
+endif
 
 PRODUCT_NAME := msmnile_gvmq
 PRODUCT_BRAND := qti
@@ -741,8 +748,10 @@ PRODUCT_PACKAGES += qcar-gsi.avbpubkey
 ifeq ($(TARGET_SINGLE_TREE), true)
   # Include mainline components and QSSI whitelist
   ifeq (true,$(call math_gt_or_eq,$(SHIPPING_API_LEVEL),29))
-    $(call inherit-product, device/qcom/qssi_au/qssi_au_whitelist.mk)
-    PRODUCT_ARTIFACT_PATH_REQUIREMENT_IGNORE_PATHS := /system/system_ext/
+    ifeq (true,$(call math_lt,$(PLATFORM_SDK_VERSION),36))
+      $(call inherit-product, device/qcom/qssi_au/qssi_au_whitelist.mk)
+      PRODUCT_ARTIFACT_PATH_REQUIREMENT_IGNORE_PATHS := /system/system_ext/
+    endif
     PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
   endif
 
