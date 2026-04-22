e0fb1ebede5528afb4a014d3804b2b26cbe685dd
Rajesh RN <rajeshra@qti.qualcomm.com>
2026-04-22 20:20:54 +0530

    msmnile_gvmq: Creating flag to differentiate Shipping level
    Creating flag to differentiate Shipping level on different
    android versions
    
    Change-Id: If6d06e051ef723463d05a633fa7ccbe650cdae83
    Signed-off-by: Rajesh RN <rajeshra@qti.qualcomm.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -328,11 +328,14 @@ endif
 #Enable VNDK Compliance
 BOARD_VNDK_VERSION:=current
 
+$(call add_soong_config_var,qti,IS_ANDROID_SHIPPING_U)
 $(call add_soong_config_var,qti,IS_ANDROID_SHIPPING_V)
 $(call add_soong_config_var,qti,IS_ANDROID_SHIPPING_W)
-ifeq ($(PLATFORM_SDK_VERSION),35)
+ifeq ($(BOARD_SHIPPING_API_LEVEL),34)
+    $(call soong_config_set,qti,IS_ANDROID_SHIPPING_U,true)
+else ifeq ($(BOARD_SHIPPING_API_LEVEL),202404)
     $(call soong_config_set,qti,IS_ANDROID_SHIPPING_V,true)
-else ifeq ($(PLATFORM_SDK_VERSION),36)
+else ifeq ($(BOARD_SHIPPING_API_LEVEL),202504)
     $(call soong_config_set,qti,IS_ANDROID_SHIPPING_W,true)
 endif
 
